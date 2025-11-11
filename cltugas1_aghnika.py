4f48000020256f498bc3b0da6d7e20c89e66965c
aghnika-crypto <lvviieacxzrra@gmail.com>
2025-11-11 13:15:56 +0700

    Add files via upload

---
+++ cltugas1_aghnika.py
@@ -0,0 +1,32 @@
+#python 3.7.1
+
+class AlatMusik:
+    pass
+
+
+alatmusik1 = AlatMusik()
+alatmusik1.nama = "Gendang"
+alatmusik1.jenis = "Tradisional"
+alatmusik1.cara_memainkan = "Dipukul"
+alatmusik1.asal_daerah = "Jawa Barat"
+alatmusik1.bahan_pembuat = "Kayu dan kulit sapi"
+
+alatmusik2 = AlatMusik()
+alatmusik2.nama = "Kecapi"
+alatmusik2.jenis = "Tradisional"
+alatmusik2.cara_memainkan = "Dipetik"
+alatmusik2.asal_daerah = "Jawa Barat"
+alatmusik2.bahan_pembuat = "Kayu dan kawat baja/nilon"
+
+
+print(alatmusik1.nama)
+print(alatmusik1.jenis)
+print(alatmusik1.cara_memainkan)
+print(alatmusik1.asal_daerah)
+print(alatmusik1.bahan_pembuat)
+
+print(alatmusik2.nama)
+print(alatmusik2.jenis)
+print(alatmusik2.cara_memainkan)
+print(alatmusik2.asal_daerah)
+print(alatmusik2.bahan_pembuat)
---
+++ cltugas2.py
@@ -0,0 +1,28 @@
+#python 3.7.1
+
+class Alatmusik:
+ 
+    nama1 = "Gendang"
+    jenis1 = "Tradisional"
+    cara_memainkan1 = "Dipukul"
+    asal_daerah1 = "Jawa Barat"
+    bahan_pembuat1 = "Kayu dan kulit sapi"
+    nama2 = "Kecapi"
+    jenis2 = "Tradisional"
+    cara_memainkan2 = "Dipetik"
+    asal_daerah2 = "Jawa Barat"
+    bahan_pembuat2 = "Kayu dan kawat baja/nilon"
+
+alatmusik1 = Alatmusik()
+alatmusik2 = Alatmusik()
+
+print(alatmusik1.nama1)
+print(alatmusik1.jenis1)
+print(alatmusik1.cara_memainkan1)
+print(alatmusik1.asal_daerah1)
+print(alatmusik1.bahan_pembuat1)
+print(alatmusik2.nama2)
+print(alatmusik2.jenis2)
+print(alatmusik2.cara_memainkan2)
+print(alatmusik2.asal_daerah2)
+print(alatmusik2.bahan_pembuat2)
