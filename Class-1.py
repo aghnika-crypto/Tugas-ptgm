3abe670f04d0f0dd672defa4759d33ff5ef6a7a6
aghnika-crypto <lvviieacxzrra@gmail.com>
2025-11-12 14:30:19 +0700

    Add files via upload

---
+++ Class-1.py
@@ -0,0 +1,35 @@
+#python 3.7.1
+
+#class
+
+class Sekolah :
+  pass
+  
+class Siswa :
+  pass
+  
+  
+sekolah1 = Sekolah()
+sekolah2 = Sekolah()
+
+print(type(sekolah1))
+print(type(sekolah2))
+
+siswa1 = Siswa()
+siswa2 = Siswa()
+
+print(type(siswa1))
+print(type(siswa2))
+
+
+sekolah1 = Sekolah()
+sekolah1.nama = "SMK PGRI 35 Solokan Jeruk"
+sekolah1.alamat = "JL RHO Kosasih no 90 Cibodas"
+sekolah2 = Sekolah()
+sekolah2.nama = "SMKN 9 Bandung"
+sekolah2.alamat = "JL Soekarno-Hatta No 10"
+
+print(sekolah1.nama)
+print(sekolah1.alamat)
+print(sekolah2.nama)
+print(sekolah2.alamat)
